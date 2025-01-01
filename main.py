5dad03e4911c31e6eb1fd55ffd410249e149e1da
Syed Masood Hussain <hmasood3288@gmail.com>
2025-01-01 13:11:55 +0500

    made three turtle, and made them together

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_position:
+    segment = Turtle("square")
+    segment.color("white")
+    segment.goto(position)
+
 
 screen.exitonclick()
